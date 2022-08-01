17bab8d1071b9147d525cc87f21ea627aeebed00
harshverma7864 <83391423+harshverma7864@users.noreply.github.com>
2022-08-02 00:21:40 +0530

    Delete Words.js

--- pages/components/Words.js
+++
@@ -1,8 +0,0 @@
-// filename: words.js
-const Words = [
-    'Welcome to GeeksLord',
-    'Your Coding Buddy',
-    'Invest Your Time Here',
-    'Receive The Best Returns'
-  ]
-  export default Words
